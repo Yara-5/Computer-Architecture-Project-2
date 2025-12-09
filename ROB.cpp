6d2d59c513bc1a360fc5634589985a0585b26020
Yara-5 <yara2005@aucegypt.edu>
2025-12-09 11:50:41 +0200

    Created ROB class, initialized hardware, instruction issuing

---
+++ ROB.cpp
@@ -0,0 +1,109 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+// ROB entry
+struct ROBEntry {
+    bool busy;
+    char type;
+    int destination;     // register or memory address
+    int16_t value;
+    bool ready;
+
+    ROBEntry(char typ, int dest, int val) {
+        busy = true;
+        type = typ;
+        destination = dest;
+        value = val;
+        ready = false;
+    }
+};
+
+
+class ROB {
+    int size;           // fixed
+    int head;
+    int tail;
+    vector<ROBEntry> entries;
+    int count;          // Number of elements currently in ROB
+
+public:
+
+    ROB(int robSize) {
+        head = 0;
+        tail = 0;
+        count = 0;
+        size = robSize;
+        entries.resize(robSize);
+    }
+
+    bool isFull() const {
+        return (count == size);
+    }
+
+    bool isEmpty() const {
+        return (count == 0);
+    }
+
+    int allocate(char type, int dest) {
+        if (isFull()) {
+            cout << "Error: ROB is full, can't allocate\n";
+            return -1;
+        }
+
+        entries[tail] = ROBEntry(type, dest, 0);
+        int index = tail;
+        tail = (tail + 1) % size;
+        count++;
+        return index;
+    }
+
+    bool findVal(int dest, int16_t& value) {
+        int index = tail;
+        do {
+            index = (index - 1) % size;
+            if (entries[index].ready && entries[index].type != 's' && entries[index].destination == dest) {
+                value = entries[index].value;
+                return true;
+            }
+
+        } while (index != head);
+        return false;
+    }
+
+    void markReady(int index, int16_t val) {
+        entries[index].ready = true;
+        entries[index].value = val;
+    }
+
+    void changeDest(int index, int dest) {      // in case of store
+        entries[index].destination = dest;
+    }
+
+    bool canCommit() const {
+        return entries[head].ready;
+    }
+
+    void commit() {
+        entries[head].busy = false;
+        if (count == 1) {
+            head = 0;
+            tail = 0;
+        }
+        else
+            head = (head + 1) % size;
+        count--;
+    }
+
+    void flushAfter(int branchIndex) {
+        while (tail != branchIndex)
+        {
+            count--;
+            tail = (tail - 1) % size;
+            entries[tail].busy = false;
+        }
+    }
+
+};
--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <cstdint>
-#include <string>
 
-using namespace std;
+#include "ROB.cpp"
 
 
 // Global Constants and Types
@@ -11,32 +7,32 @@ using namespace std;
 const int NUM_REGS = 8;
 const int MEMORY_SIZE = 65536;  // 128 KB word-addressable
 
+// Station Constants
+
+int reserve_num[] = {2, 1, 2, 1, 4, 2, 1};
+int reserve_start[7];
+int cycles_num[] = { 6,2,1,1,2,1,12 };
+
 struct Instruction {
-    string opcode;
+    char opcode;
     int dst, src1, src2;
     int16_t imm;       // offset or immediate
     int pc;            // program counter
-    int labelTarget;   // for CALL/BEQ
 };
 
-// ROB entry
-struct ROBEntry {
-    bool busy;
-    string type;
-    int destination;     // register or memory address
-    int16_t value;
-    bool ready;
-};
 
 // Reservation Station entry
 struct RSEntry {
     bool busy;
-    string op;
+    char op;
     int16_t Vj, Vk;
     int Qj, Qk;
     int robIndex;
     int executionCyclesLeft;
     int16_t address;   // for load/store
+
+    RSEntry(bool b, char o, int16_t vj, int16_t vk, int qj, int qk, int rind, int excl, int16_t add) : busy(b),
+        op(o), Vj(vj), Vk(vk), Qj(qj), Qk(qk), robIndex(rind), executionCyclesLeft(excl), address(add) {}
 };
 
 
@@ -47,7 +43,7 @@ int16_t dataMemory[MEMORY_SIZE];           // Data memory
 int16_t registers[NUM_REGS];               // Register file
 int regStatus[NUM_REGS];                   // ROB index producing reg (-1 if free)
 
-vector<ROBEntry> ROB;                       // Reorder buffer
+ROB rob(8);                                 // Initializing Reorder buffer
 vector<RSEntry> reservationStations;        // All RS entries
 
 int pc = 0;                                 // Program counter
@@ -56,17 +52,176 @@ int cycle = 0;                              // Global cycle counter
 
 // Phase 1: Initialization
 
-void loadProgram();                          // Load instructions into programMemory
-void initRegisters();                        // Initialize registers and regStatus
-void initMemory();                           // Initialize dataMemory
-void initROB();                              // Initialize ROB
-void initReservationStations();              // Initialize all RS entries
+void loadProgram();                           // Load instructions into programMemory
 
+void initRegisters() {                        // Initialize registers and regStatus
+    registers[0] = 0;
+    for (auto st : regStatus)
+        st = -1;
+}
+
+void initMemory() {                           // Initialize dataMemory
+    cout << "Do you need to enter data into the data memory?\nIf so press 1, else press 0.\n";
+    int ans;
+    int16_t address, data;
+    cin >> ans;
+    while (ans) {
+        cout << "Enter the address you want to enter data to: ";
+        cin >> address;
+        cout << "Enter the data you want to enter at address " << address << ": ";
+        cin >> data;
+        dataMemory[address] = data;
+        cout << "\nDo you want to enter more data?\n If yes, press 1, else press 0\n";
+    }
+    cout << "\nThe program will start running now.\n";
+}
+
+void initReservationStations() {              // Initialize all RS entries
+    for (auto rs : reservationStations)
+        rs.busy = false;
+}
 
 // Phase 2: Issue
 
-bool canIssue(const Instruction& inst);
-void issueInstruction(const Instruction& inst);
+bool canIssue(const Instruction& inst, int& i) {
+    if (rob.isFull())
+        return false;
+    switch (inst.opcode) {
+    case 'l':
+        i = reserve_start[0];
+        while (reserve_start[1] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 't':
+        i = reserve_start[1];
+        while (reserve_start[2] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 'b':
+        i = reserve_start[2];
+        while (reserve_start[3] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 'c':
+        i = reserve_start[3];
+        while (reserve_start[4] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 'r':
+        i = reserve_start[3];
+        while (reserve_start[4] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 'a':
+        i = reserve_start[4];
+        while (reserve_start[5] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 's':
+        i = reserve_start[4];
+        while (reserve_start[5] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 'n':
+        i = reserve_start[5];
+        while (reserve_start[6] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    case 'm':
+        i = reserve_start[6];
+        while (reserve_start[6] + reserve_num[6] > i) {
+            if (!reservationStations[i].busy) {
+                return true;
+                break;
+            }
+            i++;
+        }
+        break;
+    }
+    return false;
+}
+
+void issueInstruction(const Instruction& inst) {
+    int ind;
+    if (!canIssue(inst, ind))
+        return;
+    int rbInd = rob.allocate(inst.opcode, inst.dst);
+    int16_t val1, val2;
+    if (!rob.findVal(inst.src1, val1))
+        val1 = registers[inst.src1];
+    if (!rob.findVal(inst.src2, val2))
+        val2 = registers[inst.src2];
+    switch (inst.opcode) {
+    case 'l':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[0], inst.imm);
+        break;
+    case 't':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[1], inst.imm);
+        break;
+    case 'b':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[2], inst.pc + inst.imm);
+        break;
+    case 'c':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[3], inst.pc + inst.imm);
+        break;
+    case 'r':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[3], inst.imm);
+        break;
+    case 'a':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[4], inst.imm);
+        break;
+    case 's':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[4], inst.imm);
+        break;
+    case 'n':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[5], inst.imm);
+        break;
+    case 'm':
+        reservationStations[ind] = RSEntry(true, inst.opcode, val1, val2, regStatus[inst.src1], regStatus[inst.src2], rbInd, cycles_num[6], inst.imm);
+        break;
+    }
+    pc++;
+}
 
 
 // Phase 3: Execute
@@ -99,12 +254,12 @@ void printResults();
 // Phase 7: Simulator Loop
 
 void runSimulator() {
-    while (/* ROB not empty or instructions left */) {
+    while (!rob.isEmpty()) {
         commitInstruction();
         writeBackResults();
         startExecutionForReadyRS();
         decrementExecutionTimers();
-        if (/* instructions left to issue */)
+        if (pc<programMemory.size())
             issueInstruction(programMemory[pc]);
         cycle++;
     }
@@ -119,7 +274,6 @@ int main() {
     loadProgram();
     initRegisters();
     initMemory();
-    initROB();
     initReservationStations();
 
     runSimulator();
